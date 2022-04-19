cb8e8f838ee09a41c8c4b44fcf46029652593db1
mikoto2000 <mikoto2000@gmail.com>
2022-04-20 01:44:22 +0900

    Added html reporter setting.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "hello"
 
 require "minitest/autorun"
+
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::HtmlReporter.new(:reports_dir => './reports/test')
+
